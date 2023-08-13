f52a9534b289a06b8d4d55edc1fdedd7bea55d1b
ryan-01-git <77622772+ryan-01-git@users.noreply.github.com>
2023-08-14 00:40:55 +0530

    Add files via upload

---
+++ Portfolio-main/src/components/work/Data.jsx
@@ -0,0 +1,69 @@
+import Work1 from "../../assets/work1.png";
+import Work2 from "../../assets/work2.png";
+import Work3 from "../../assets/work3.png";
+import Work4 from "../../assets/work4.png";
+import Work5 from "../../assets/work5.png";
+import Work6 from "../../assets/work6.png";
+export const projectsData = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Saalve",
+    category: "web",
+    deploymentLink: "https://saalve.in/",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Text Analyzer",
+    category: "web",
+    deploymentLink: "https://text-analyzer-nine.vercel.app/",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Snake Game",
+    category: "web",
+    deploymentLink: "https://dishant-62.github.io/Snake-Game/",
+  },
+  {
+    id: 4,
+    image: Work4,
+    title: "Food Search Tool(API)",
+    category: "web",
+    deploymentLink: "https://dishant-62.github.io/Food-Receipe-Search-Tool/",
+  },
+  {
+    id: 5,
+    image: Work5,
+    title: "E-commerce Site",
+    category: "web",
+    deploymentLink: "https://dishant-62.github.io/Shopforclothes/",
+  },
+  {
+    id: 6,
+    image: Work6,
+    title: "Login Page UI",
+    category: "design",
+    deploymentLink: "https://uidishant.netlify.app/",
+  },
+  // {
+  //   id: 7,
+  //   image: Work5,
+  //   title: "Login Bootstrap UI",
+  //   category: "design",
+  //   deploymentLink: "https://login-via-bootstrap.netlify.app/",
+  // },
+];
+
+export const projectsNav = [
+    {
+        name: "all",
+      },
+  {
+    name: "web",
+  },
+  {
+    name: "design",
+  },
+];
